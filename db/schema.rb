ef2aa291b0a61b791f4751cfdf21a73c49dcb5b2
Snejp <snejpik@gmail.com>
2018-07-31 02:07:03 +0200

    change to PostgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_07_30_213438) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "quotes", force: :cascade do |t|
     t.text "text"
     t.datetime "created_at", null: false
